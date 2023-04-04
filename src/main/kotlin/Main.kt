dace05d259fd6ad38378d35a03f9a745f221a76e
Antônia Turcatto <lucasturcatto.va048@academico.ifsul.edu.br>
2023-04-04 19:47:29 -0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,27 @@
+fun main() {
+    //Questão 1
+    var i=1
+    while(i<=10){
+        println(i)
+        i++
+    }
+    println()
+
+    //Questão 2
+    var x = 0
+    while (x <= 20){
+        println(x)
+        x+=2
+    }
+    println()
+
+    //Questão 3
+    var contador = 10
+    while (contador >= 0){
+        if(contador%2!=0){
+            println(contador)
+        }
+        contador--
+    }
+
+}
